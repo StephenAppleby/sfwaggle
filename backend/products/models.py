497b84a5ab52af1bfea4f5588d6d85df4ca1dca9
StephenAppleby <stephen.appleby27@gmail.com>
2022-08-24 10:48:40 +1000

    Implemented Product, Category, Brand models

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Brand(models.Model):
+    name = models.TextField(max_length=255)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Category(models.Model):
+    name = models.TextField(max_length=255)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Product(models.Model):
+    name = models.TextField(max_length=255)
+    # image = models.SOMETHINGSOMETHING
+    description = models.TextField(max_length=1023)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    count_in_stock = models.PositiveSmallIntegerField()
+    # rating and numReviews can be calculated programatically once we implement reviews
+
+    def __str__(self):
+        return str(self.name)
