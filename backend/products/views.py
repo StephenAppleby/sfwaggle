0c3764b64f5a8adb22586116fd40a0f54ee58da9
StephenAppleby <stephen.appleby27@gmail.com>
2022-10-30 11:13:41 +1100

    Use django.http.Http404 in products.views

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,7 +1,9 @@
+from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 from .models import CartItem, Product
 from .serializers import CartItemSerializer, ProductSerializer
 from rest_framework import status
@@ -16,8 +18,11 @@ class ProductListView(APIView):
 
 class ProductView(APIView):
     def get(self, request, pk=None):
-        serializer = ProductSerializer(Product.objects.get(pk=pk))
-        return Response(serializer.data)
+        try:
+            serializer = ProductSerializer(Product.objects.get(pk=pk))
+            return Response(serializer.data)
+        except Product.DoesNotExist as e:
+            raise Http404(e)
 
 
 class CartView(APIView):
@@ -33,12 +38,12 @@ class CartView(APIView):
             request.user.cart_items.add(serializer.instance)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            raise ValidationError(serializer.errors)
 
     def get(self, request):
         cart_items = request.user.cart_items.all()
         serializer = CartItemSerializer(cart_items, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def put(self, request):
         try:
@@ -57,7 +62,7 @@ class CartView(APIView):
         except Product.DoesNotExist:
             raise serializers.ValidationError("Bad product uuid")
         except CartItem.DoesNotExist:
-            return Response("Product is not in cart", status=status.HTTP_404_NOT_FOUND)
+            raise Http404("Product is not in cart")
 
     def delete(self, request):
         try:
@@ -70,4 +75,4 @@ class CartView(APIView):
         except Product.DoesNotExist:
             raise serializers.ValidationError("Bad product uuid")
         except CartItem.DoesNotExist:
-            return Response("Product is not in cart", status=status.HTTP_404_NOT_FOUND)
+            raise Http404("Product is not in cart")
